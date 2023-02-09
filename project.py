fee47d76d4de537ab6112410a29b6cf75fba20c5
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-02-08 22:09:01 -0300

    att
    att

--- project.py
+++ project.py
@@ -32,3 +32,46 @@ display(ca_df.loc[3])
 
 #Exibe da 10a. linha até a 20a. linha
 display(ca_df.loc[9:19])
+
+# Criar um dataframe gas_df contendo 
+# apenas as 4 colunas (Revenda, Municipio, Produto, Valor de Venda)
+# somente com combustível sendo GASOLINA e exibir na tela
+gas_df = ca_df.loc[ca_df['Produto'] == 'GASOLINA']
+display(gas_df)
+
+display(gas_df['Valor de Venda'].max())
+
+#DataFrame.loc[] com múltiplas condições para filtragem
+#Quais são os preços, postos que vendem ETANOL na minha cidade (INDAIATUBA) 
+#ordenado do menor valor de venda para o maior
+etanol_indaiatuba_df = ca_df.loc[(ca_df['Produto'] == 'ETANOL') & (ca_df['Municipio'] == 'INDAIATUBA')]
+display(etanol_indaiatuba_df.sort_values(by='Valor de Venda'))
+
+# Qual a média de preços dos combustíveis GASOLINA e GASOLINA ADITIVADA do Bairro MOOCA em SÃO PAULO?
+display(combustiveis_df.loc[(combustiveis_df['Bairro'] == 'MOOCA') & (combustiveis_df['Municipio'] == 'SAO PAULO') & ((combustiveis_df['Produto'] == 'GASOLINA') | (combustiveis_df['Produto'] == 'GASOLINA ADITIVADA')), ['Valor de Venda']].mean())
+
+# Qual a média de preços dos combustíveis GASOLINA e GASOLINA ADITIVADA do Bairro MOOCA em SÃO PAULO?
+display(combustiveis_df.loc[(combustiveis_df['Bairro'] == 'MOOCA') & 
+                            (combustiveis_df['Municipio'] == 'SAO PAULO') & 
+                            ((combustiveis_df['Produto'] == 'GASOLINA') | (combustiveis_df['Produto'] == 'GASOLINA ADITIVADA')), 
+                            ['Valor de Venda']].mean())
+
+# Qual a média de preços dos combustíveis GASOLINA e GASOLINA ADITIVADA do Bairro MOOCA em SÃO PAULO?
+display(combustiveis_df.loc[(combustiveis_df['Bairro'] == 'MOOCA') & 
+                            (combustiveis_df['Municipio'] == 'SAO PAULO') & 
+                            (combustiveis_df['Produto'].isin(["GASOLINA", "GASOLINA ADITIVADA"])), 
+                            ['Valor de Venda']].mean())
+
+# Como mostrar média de valor de venda POR COMBUSTÍVEL Brasil todo?
+media_por_combustivel_df = ca_df[['Produto', 'Valor de Venda']].groupby(by='Produto').mean().round(2)
+display(media_por_combustivel_df)
+
+# Quero adicionar uma coluna de valor booleano no combustiveis_df
+# chamada "Ativo" que, por padrão, vai ser True para 
+# TODAS as linhas
+combustiveis_df['Ativo'] = True
+print(combustiveis_df.info())
+display(combustiveis_df.head())
+
+# Exportar para Excel o dataframe com etanol em Indaiatuba....
+etanol_indaiatuba_df.to_excel('etanol_indaiatuba.xlsx', sheet_name='Etanol em Indaiatuba')
